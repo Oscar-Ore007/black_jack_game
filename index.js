bdd2e62610360bfcdaba18bb7068e76ab2a9b90c
Oscar Ore <oscar.ore2011@gmail.com>
2022-02-07 11:15:20 -0500

    added sum to display on screen

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+
 function startGame() {
+    sumEl.textContent = "Sum:" + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card? "
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "Wohoo! You've got Blackjack! "
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game! "
         isAlive = false
     }
-    console.log(message)
+   messageEl.textContent = message 
+ 
 }
 
 
