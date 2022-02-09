e99dd4a0b82c32d7c0c9d0ae9352fae7b715321a
Oscar Ore <oscar.ore2011@gmail.com>
2022-02-09 16:36:55 -0500

    added random card functionality

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+
+
+let cards = [firstCard, secondCard] //ordered list of items 
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -9,8 +12,28 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-function startGame() {
-    cardsEl.textContent =  "Cards: " + firstCard + " " + secondCard
+//made the function return a random number between 1 and 13 
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1 
+    if ( randomNumber > 10) {
+        return 10
+    } else if(randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame () {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent =  "Cards: " 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    //replaced with array 
     sumEl.textContent = "Sum:" + sum
   
     if (sum <= 20) {
@@ -23,10 +46,17 @@ function startGame() {
         isAlive = false
     }
    messageEl.textContent = message 
+}
  
    function newCard() {
        console.log("Drawing a new card from the deck!")
+
+       let card = getRandomCard()
+       sum += card 
+       cards.push(card)
+       console.log(cards)
+       renderGame()
    }
-}
+
 
 
